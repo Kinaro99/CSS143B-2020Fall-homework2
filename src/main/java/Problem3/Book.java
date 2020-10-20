ff7853d0fca92479d19c7bf265fbeb6afc7d5c57
Kinaro99 <abbie687@uw.edu>
2020-10-19 23:15:24 -0700

    Problem 3 done and tests added

--- src/main/java/Problem3/Book.java
+++ src/main/java/Problem3/Book.java
@@ -15,11 +15,29 @@ public abstract class Book implements StoreMediaOperations {
 
     // copy constructor
     public Book(Book anotherBook) {
-        // homework
+        this.title = anotherBook.title;
+        this.author = anotherBook.author;
+        this.id = anotherBook.id;
     }
 
-    @Override
+    @Override // https://www.baeldung.com/java-equals-hashcode-contracts and https://www.javatpoint.com/java-string-equals
     public boolean equals(Object obj) {
-        // homework
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Book)) {
+            return false;
+        }
+        Book anotherBook = (Book)obj;
+        //boolean titleEquals = (this.title == null && other.title == null) || (this.title != null && this.title.equals(other.title));
+        //boolean authorEquals = (this.author == null && other.author == null) || (this.author != null && this.author.equals(other.author));
+        // books that have the same ID have have the same author and title so the top 2 wouldn't be needed(?).
+
+        boolean idEquals
+                = (this.id == null && anotherBook.id == null) || (this.id != null && this.id.equals(anotherBook.id));
+            return this.title == anotherBook.title && idEquals;
     }
+
+
+
 }
